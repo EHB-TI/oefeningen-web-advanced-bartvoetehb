0e9404ad0bada742038351460bacee6ec65734f6
bartvoetb <bart.voet@student.ehb.be>
2022-04-20 21:31:08 +0200

    S1 part 3 (requesting age)

---
+++ s1/e3/js/script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let age = parseInt(prompt("What's your age?","18"))
+
+if (age >= 18) {
+    console.log("Welcome!")
+} else {
+    console.log(`Sorry, ${age} is to young, this is a 18+ website`)
+}
