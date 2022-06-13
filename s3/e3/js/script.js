93689b85c789dad50f58b4ca78e86f3ad4439034
bartvoetb <bart.voet@student.ehb.be>
2022-06-13 09:16:03 +0200

    s3 e3 Adding bootstrap-alert

--- s3/e3/js/script.js
+++ s3/e3/js/script.js
@@ -18,16 +18,36 @@ window.onload = function() {
         };
     }
 
-     function addStudent(event) {
+    function clearFields() {
+        document.getElementById("name").value = '';
+        document.getElementById("age").value = '';
+    }
+
+    function showAlert(success, message) {
+        let result = success ? "success" : "danger";
+        
+        let content = document.createElement("div");
+        content.setAttribute("class", `alert alert-${result}`);
+        content.setAttribute("role","alert");
+        content.innerText = message;
+        
+        document.getElementById("messages").innerHTML = "";
+        document.getElementById("messages").appendChild(content);
+    }
+
+    function addStudent(event) {
         let name = document.getElementById("name").value;
         let age = document.getElementById("age").value;
 
-        let student = new Student(name, age);
-        studenten.push(student);
-
-        document.getElementById("name").value = '';
-        document.getElementById("age").value = '';
-
+        if(name === '' || age === ''){
+            showAlert(false,"Either name or age was not inserted");
+        } else {
+            let student = new Student(name, age);
+            studenten.push(student);
+            showAlert(true,"Student created: " + student.showStudent());
+            
+        }
+        clearFields();
     }
 
     let regForm = document.getElementById("registerForm");
