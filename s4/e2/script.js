2f1444c938da122029ec6d2f8e39ab1f2a9ca32b
bartvoetb <bart.voet@student.ehb.be>
2022-06-15 23:40:40 +0200

    s4 e2 Add bootstrap-form + isolate search in function

--- s4/e2/script.js
+++ s4/e2/script.js
@@ -5,8 +5,15 @@ let apiKey = 'cceb1d2d';
 window.onload = async function(event) {
     console.log(apiKey);
     let title = "parasite";
+    
     let response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`);
-    let data = await response.json();
+    let data = await searchMovie(title);
     //console.log(data);
     document.getElementById("content").innerText = JSON.stringify(data);
+
+    async function searchMovie(title) {
+        let response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`);
+        let data = await response.json();
+        return data;
+    }
 };
