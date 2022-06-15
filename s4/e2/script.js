4e6227bea883f511068b08880ceb5856e87b996a
bartvoetb <bart.voet@student.ehb.be>
2022-06-15 23:53:52 +0200

    s4 e2 Search based on input

--- s4/e2/script.js
+++ s4/e2/script.js
@@ -3,17 +3,19 @@ let apiKey = 'cceb1d2d';
 //http://www.omdbapi.com/?t=parasite
 
 window.onload = async function(event) {
-    console.log(apiKey);
-    let title = "parasite";
-    
-    let response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`);
-    let data = await searchMovie(title);
-    //console.log(data);
-    document.getElementById("content").innerText = JSON.stringify(data);
-
     async function searchMovie(title) {
+        console.log("hello");   
         let response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`);
         let data = await response.json();
         return data;
     }
+
+    document.getElementById("movieSearch").addEventListener("submit",
+        async function(event) {
+            event.preventDefault();
+            let movieTitle = document.getElementById("movieTitle").value;
+            data = await searchMovie(movieTitle);
+            document.getElementById("content").innerText = JSON.stringify(data);
+        }
+    );
 };
