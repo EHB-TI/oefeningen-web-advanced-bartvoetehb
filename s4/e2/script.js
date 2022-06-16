ff3e6222a630cbd74ac362ccb942ccb9d4a6bd89
bartvoetb <bart.voet@student.ehb.be>
2022-06-16 12:16:49 +0200

    s4 e2 Adding search on keyup (and grouplist)

--- s4/e2/script.js
+++ s4/e2/script.js
@@ -1,9 +1,13 @@
+'use strict'
+
 let apiKey = 'cceb1d2d';
 
 //http://www.omdbapi.com/?t=parasite
 
 let currenData = null;
 let count = 0;
+
+const baseurl = "http://www.omdbapi.com/"
 
 window.onload = async function(event) {
 
@@ -18,13 +22,29 @@ window.onload = async function(event) {
     }
 
     let setCardVisibility = (visible) => setVisibility("content", visible);
+    let setSearchListVisbility  = (visible) => setVisibility("searchList", visible);
 
+    
     setCardVisibility(false);
+    //setSearchListVisbility(false);
+
+    function urlWithQuery() {
+        return `${baseurl}?apikey=${apiKey}` 
+    }
 
     async function searchMovie(title) {
         //let response = await fetch("stubdata.json"); //avoiding to many calls for now while developping card
-        let response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`);
-        console.log(response.status);
+        let response = await fetch(`${urlWithQuery()}&t=${title}`);
+        if(response.ok) {
+            return await response.json();
+        } else {
+            return null;
+        }
+    }
+
+    async function searchMovies(title) {
+        let response = await fetch(`${urlWithQuery()}&s=${title}`);
+        //let response = await fetch("stubsearchdata.json");
         if(response.ok) {
             return await response.json();
         } else {
@@ -44,10 +64,9 @@ window.onload = async function(event) {
         }
     }
 
-    async function setMovie() {
+    async function setMovie(movieTitle) {
         //title, year, director, poster, runtime
-        let movieTitle = document.getElementById("movieTitle").value;
-        data = await searchMovie(movieTitle);
+        let data = await searchMovie(movieTitle);
         console.log(JSON.stringify(data));
         if(data) {
             currenData = data;
@@ -58,9 +77,38 @@ window.onload = async function(event) {
     document.getElementById("movieSearch").addEventListener("submit",
         async function(event) {
             event.preventDefault();
-            setMovie();
+            setMovie(document.getElementById("movieTitle").value);
         }
     );
+
+    document.getElementById("movieTitle").addEventListener("keyup",
+        async function(event) {
+            let data = await searchMovies(event.target.value);
+            // console.log(data);
+            // console.log(JSON.stringify(data));
+
+            let resultList = data.Search;
+            console.log(resultList);
+
+            let pos = 0;
+
+            let searchList = document.getElementById("searchList");
+            for (let child of searchList.children) {
+                let movie = resultList[pos];
+                child.innerText = movie.Title;
+                pos = pos + 1;
+            }
+
+        }
+    );
+
+
+    let searchList = document.getElementById("searchList");
+    for (let child of searchList.children) {
+        child.addEventListener("click",function(event){
+            setMovie(event.target.innerText);
+        });
+    }
 
     document.getElementById("plusButton").addEventListener("click", 
         async function(event) {
